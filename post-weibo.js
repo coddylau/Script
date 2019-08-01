7d43b2613120892b9c4c7cc1f1cd43d67152c521
Cody <1609336823@qq.com>
2019-08-01 13:02:54 +0800

    火狐浏览器bugfix

--- post-weibo.js
+++ post-weibo.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         发送仅自己可见微博
-// @version      0.1
+// @version      0.2
 // @description  默认发送仅自己可见微博
 // @author       coddylau
 // @match        https://weibo.com/*
@@ -12,10 +12,17 @@
 (function () {
   "use strict";
   window.onload = function () {
+    let postBtn = document.querySelector('.func_area')
+
     document.querySelector('.limits .S_txt1').click()
-    setTimeout(() => {
+    
+    // 监视发送微博子节点
+    let cb = () => {
       document.querySelector('.layer_menu_list').style.display = 'none'
-    }, 200);
+    }
+    let observer = new MutationObserver(cb)
+    observer.observe(postBtn, { childList: true })
+
     document.querySelectorAll(".W_input")[1].addEventListener('change', () => {
       if (document.querySelectorAll('li[action-type="select"]')[2].classList[0] === 'cur') { return }
       let flag = 1
